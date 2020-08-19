a7615884b4ca001a66133efaf855c839e603e6c6
Sushmasreeg <68965860+Sushmasreeg@users.noreply.github.com>
2020-08-19 16:23:08 +0530

    Add files via upload

---
+++ game.js
@@ -0,0 +1,76 @@
+buttonColors=["red","blue","green","yellow"];
+gamePattern=[];
+
+var started=false;
+var level=0;
+
+$(document).on("keydown",function(){
+  if(!started){
+      $("h1").text("Level "+level);
+      nextSequence();
+      started=true;
+  }
+});
+
+function nextSequence(){
+  userClickedPattern=[];
+  level++;
+  $("h1").text("Level "+level);
+
+  var n= Math.random();
+  var randomNumber=Math.floor(n*4);
+  var randomChosenColor=buttonColors[randomNumber];
+
+  gamePattern.push(randomChosenColor);
+
+  $("#"+randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColor);
+
+
+}
+
+
+  $(".btn").on("click",function(){
+
+    var userChosenColor=this.id;
+    playSound(userChosenColor);
+    animatePress(this);
+    userClickedPattern.push(userChosenColor);
+    checkAnswer(userClickedPattern.length-1);
+  });
+
+
+function playSound(name){
+  var sound=new Audio("sounds/"+name+".mp3");
+  sound.play();
+}
+
+function animatePress(currentColor){
+  $(currentColor).addClass("pressed");
+  setTimeout(function(){ $(currentColor).removeClass("pressed");},100);
+}
+
+
+
+function checkAnswer(currentLevel)
+{
+  if(userClickedPattern[currentLevel]===gamePattern[currentLevel])
+  {
+    console.log("success");
+    if(userClickedPattern.length===gamePattern.length)
+    {
+      setTimeout(function(){ nextSequence()}, 1000);
+    }
+  }
+  else{
+    var sound=new Audio("sounds/wrong.mp3");
+    sound.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){$("body").removeClass("game-over")},200);
+    $("h1").text("Game Over, Press Any Key To Restart");
+    gamePattern=[];
+    started=false;
+    level=0;
+  }
+}
